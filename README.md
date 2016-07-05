7a4b123b6c58bd038b48b2de0df4fb1870d524a2
Suellen Tobler <tobler.suellen@gmail.com>
2016-07-04 21:30:11 -0300

    versionando projeto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Desafio Frontend 001
+
+### Web App client-side, which imports data using Dribbble API.
+
+link: http://tobler.com.br/desafio-frontend-001
+
+---
+AngularJS
+Bootstrap
+HTML5
+CSS3
+
+
---
+++ js/app.js
@@ -0,0 +1,18 @@
+var myApp = angular.module('myApp', ['ngSanitize']);
+var token = "bcb5944c2d20f435e91068e9673f477180a2b26c5c02d406284b6de4039ca164";
+
+myApp.controller('listController', function($scope, $http, $sce){
+
+	var rs = $http.get("https://api.dribbble.com/v1/shots?sort=viewsuser&access_token="+token);
+	rs.success(function (response) {
+	    $scope.shots = response;
+	});
+
+});
+
+
+
+
+
+
+
