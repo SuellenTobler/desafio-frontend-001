102946024292d5ce981c0da2d02d5f5a98bbeecc
Suellen Tobler <tobler.suellen@gmail.com>
2016-07-05 18:18:19 -0300

    cleaning the coding

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', ['ngSanitize']);
 var token = "bcb5944c2d20f435e91068e9673f477180a2b26c5c02d406284b6de4039ca164";
 
-myApp.controller('listController', function($scope, $http, $sce){
+myApp.controller('listController', function($scope, $http){
 
 	var rs = $http.get("https://api.dribbble.com/v1/shots?sort=viewsuser&access_token="+token);
 	rs.success(function (response) {
